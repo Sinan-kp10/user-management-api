98329c0af8865124f23df0eeae1895959759aedb
Sinan-kp10 <sinankp9503@gmail.com>
2026-09-01 11:19:34 +0530

    feat : Added register functionalities

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import adminRoutes from "./routes/admin.routes";
 import userRoutes from "./routes/user.routes";
+import authRoutes from "./routes/auth.routes";
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/admin", adminRoutes);
-app.use("/user", userRoutes);
+app.use("/admin", adminRoutes)
+app.use("/user", userRoutes)
+app.use("/auth", authRoutes)
 
 export default app;
