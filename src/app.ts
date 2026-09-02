7fa6664392ebaa185ef0cbd4c8c6877db4868f21
Sinan-kp10 <sinankp9503@gmail.com>
2026-09-02 12:50:09 +0530

    fix : Datbase connection error

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import adminRoutes from "./routes/admin.routes";
-import userRoutes from "./routes/user.routes";
 import authRoutes from "./routes/auth.routes";
 
 const app = express();
@@ -8,7 +7,6 @@ const app = express();
 app.use(express.json());
 
 app.use("/admin", adminRoutes)
-app.use("/user", userRoutes)
 app.use("/auth", authRoutes)
 
 export default app;
